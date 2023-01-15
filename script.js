9c22d6f4a7e77cd6449332e2bde58291a8c52cf0
Alexia Fast <106492503+purealexiafast@users.noreply.github.com>
2023-01-14 20:17:51 -0800

    Created Local Storage

--- script.js
+++ script.js
@@ -1,8 +1,4 @@
 
-
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
 $(function () {
   var currentHour = dayjs().hour();
   var formatDate = dayjs().format('DD/MM/YYYY ha')
@@ -12,13 +8,10 @@ $(function () {
 
   console.log(currentHour)
 
+  //Creating every block container, button, text area and formated the time with am and pm
+
   for (var i = 9; i < 17; i++) {
-
-    let createDiv = $(' <div class="row time-block"></div>')
-    let time = $('<p class="col-2 col-md-1 hour text-center py-3"></p>')
-    let textArea = $('<textarea class="col-8 col-md-10 description" rows="3"> </textarea>')
-    let button = $('<button class="btn saveBtn col-2 col-md-1" aria-label="save"><i class="fas fa-save" aria-hidden="true"></i></button>')
-
+    
     let formatTime;
     if (i < 12){
       formatTime = i + "am"
@@ -27,10 +20,17 @@ $(function () {
     } else {
       formatTime = i + "pm"
     }
+    let createDiv = $(' <div class="row time-block"></div>')
+    let time = $('<p class="col-2 col-md-1 hour text-center py-3"></p>')
+    let textArea = $('<textarea id="at'+ formatTime + '" class="col-8 col-md-10 description" rows="3"> </textarea>')
+    let button = $('<button class="btn saveBtn col-2 col-md-1" aria-label="save"><i class="fas fa-save" aria-hidden="true"></i></button>')
+
 
     time.text(formatTime)
     createDiv.append(time, textArea, button);
     container.append(createDiv);
+
+    //if statement determining the color of the text area depending on the time of day
 
     if (currentHour === i) {
       createDiv.addClass("present");
@@ -41,46 +41,72 @@ $(function () {
     
     }
 
+    //Saving user input to local storage
+
     button.on("click", function(event){
       
       var textToSave = button.siblings()[1].value; 
       localStorage.setItem(formatTime, textToSave);
-      console.log(textToSave);
-      textToSave.textContent();
-    })
-  }
+  
+      })
+      
+    }
 
+    //Displaying user input even after the page refreshes
 
+    let userInput = "";
 
+    userInput = localStorage.getItem('9am');
+    console.log(userInput);
+    var the9AmContainer = $("#at9am")
+    console.log(the9AmContainer)
+    the9AmContainer.val(userInput);
+
+    userInput = localStorage.getItem('10am');
+    console.log(userInput);
+    var the10AmContainer = $("#at10am")
+    console.log(the10AmContainer)
+    the10AmContainer.val(userInput);
+
+    userInput = localStorage.getItem('11am');
+    console.log(userInput);
+    var the11AmContainer = $("#at11am")
+    console.log(the11AmContainer)
+    the11AmContainer.val(userInput);
+
+    userInput = localStorage.getItem('12pm');
+    console.log(userInput);
+    var the12pmContainer = $("#at12pm")
+    console.log(the12pmContainer)
+    the12pmContainer.val(userInput);
+
+    userInput = localStorage.getItem('1pm');
+    console.log(userInput);
+    var the1pmContainer = $("#at1pm")
+    console.log(the1pmContainer)
+    the1pmContainer.val(userInput);
+
+    userInput = localStorage.getItem('2pm');
+    console.log(userInput);
+    var the2pmContainer = $("#at2pm")
+    console.log(the2pmContainer)
+    the2pmContainer.val(userInput);
+
+    userInput = localStorage.getItem('3pm');
+    console.log(userInput);
+    var the3pmContainer = $("#at3pm")
+    console.log(the3pmContainer)
+    the3pmContainer.val(userInput);
+
+    userInput = localStorage.getItem('4pm');
+    console.log(userInput);
+    var the4pmContainer = $("#at4pm")
+    console.log(the4pmContainer)
+    the4pmContainer.val(userInput);
 
 });
 
 
-
-
-
-
-
-
-
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
 
 
 
